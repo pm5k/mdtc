220efcd82e36723cfd2a3609f54fe37368b15ea0
pm5k <tom.daukintis@gmail.com>
2023-02-16 21:39:43 +0000

    Added MPLv2 headers to each source code file.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 from collections.abc import Generator
 from dataclasses import dataclass
 from typing import Any, Type, TypeAlias
